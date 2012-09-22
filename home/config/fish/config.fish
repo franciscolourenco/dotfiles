a10bd901713cc27298c9894d706e0442042dc717
Francisco <git@franciscolour.com>
2012-09-22 21:57:38 +0100

    Add fishfish

---
+++ home/config/fish/config.fish
@@ -0,0 +1,40 @@
+if status --is-login
+
+    # source file with personal variables
+    if [ -f '~/.config/fish/secrets.fish' ]
+        . ~/.config/fish/secrets.fish
+    end
+
+    # complete path
+    set -x PATH /usr/local/bin /usr/local/sbin $PATH .
+
+    # add personal binaries to path
+    if [ -d $HOME/Dropbox/bin ]
+        set -x PATH $PATH "$HOME/Dropbox/bin"
+    end
+
+end
+
+if status --is-interactive
+
+    # set e to sublime if available, otherwise use nano
+    if [ -f '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' ]
+        set -x PATH $PATH /Applications/Sublime Text 2.app/Contents/SharedSupport/bin
+        function e
+            if [ $argv ]
+                'subl' -n $argv
+            else
+                'subl' .
+            end
+        end
+    else
+        function e
+            if [ $argv ]
+                nano -n $argv
+            else
+                nano .
+            end
+        end
+    end
+
+end
