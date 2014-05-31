4b23afc8cb6ae2e11dde853267cb54e7b9018cd6
Francisco <git@franciscolour.com>
2014-05-31 20:45:45 +0100

    Update virtualfish
    Update to last version
    Improve prompt
    Use new auto activation system

--- home/config/fish/functions/fish_prompt.fish
+++ home/config/fish/functions/fish_prompt.fish
@@ -5,6 +5,8 @@ end
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
 
+  echo ''
+
   # User
   set_color $fish_color_user
   echo -n (whoami)
@@ -24,12 +26,12 @@ function fish_prompt --description 'Write out the prompt'
   echo -n (home_pwd)
   set_color normal
 
-  echo -n ' '
+
   __terlar_git_prompt
-  echo -n ' '
+
 
   if set -q VIRTUAL_ENV
-      echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+      echo -n -s ' with ' (set_color -b normal blue) (basename "$VIRTUAL_ENV") (set_color normal)
   end
 
   echo
