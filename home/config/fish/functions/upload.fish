0abc5b682f8f4176458089c8ea6081c8d0acf99e
Francisco <git@franciscolour.com>
2015-02-09 18:48:47 +0000

    Fix upload.fish to support spaces in files names

--- home/config/fish/functions/upload.fish
+++ home/config/fish/functions/upload.fish
@@ -1,7 +1,7 @@
 function upload --description 'Upload a file to transfer.sh'
     # write url to tmpfile because of progress bar
     set tmpfile (mktemp -t transfer.XXX)
-    curl --progress-bar --upload-file $argv https://transfer.sh/(basename $argv) >> $tmpfile
+    curl --progress-bar --upload-file $argv https://transfer.sh/(basename $argv | tr -d [:space:]) >> $tmpfile
     cat $tmpfile
     # copy to clipboard if on OS X
     if type pbcopy > /dev/null
