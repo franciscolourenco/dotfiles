1de07c1c17a35befe16a22168674c78898d3875a
Francisco Lourenço <francisco@framer.com>
2025-06-08 09:44:33 +0200

    Add macOS preferences

---
+++ macos.sh
@@ -0,0 +1,5 @@
+# Light Trackpad click
+defaults write com.apple.AppleMultitouchTrackpad "FirstClickThreshold" -int "0"
+
+# Show all extensions in Finder
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
